5ce8c6940e619a0fe4c45369db9b7bcc0986af24
Marcin <marcin.krol2004@onet.pl>
2025-07-17 18:29:45 +0200

    initial commit

---
+++ src/main/java/com/githubviewer/github_api/GitHubViewerApplication.java
@@ -0,0 +1,13 @@
+package com.githubviewer.github_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubviewer/github_api/GitHubViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubviewer.github_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
