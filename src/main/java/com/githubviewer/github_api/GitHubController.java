2b382d2b67a43d98573e26ef45b3b8429de62db6
Marcin <marcin.krol2004@onet.pl>
2025-07-18 23:43:50 +0200

    add 404 response when GitHub user does not exist

--- src/main/java/com/githubviewer/github_api/GitHubController.java
+++ src/main/java/com/githubviewer/github_api/GitHubController.java
@@ -1,5 +1,6 @@
 package com.githubviewer.github_api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,17 @@ public class GitHubController {
         this.gitHubService = gitHubService;
     }
 
+
+
+
     @GetMapping("/{username}")
-public ResponseEntity<?> getUserRepositories(@PathVariable String username){
-        return ResponseEntity.ok(gitHubService.getRepositories(username));
+    public ResponseEntity<?> getUserRepositories(@PathVariable String username) {
+        try {
+            return ResponseEntity.ok(gitHubService.getRepositories(username));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse(404, String.format("User '%s' not found", username)));
+        }
     }
+    public record ErrorResponse(int status, String message) {}
 }
